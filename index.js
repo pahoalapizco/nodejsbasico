d1936582f843a177fdd54b124cf4f7ad795ecfe0
Paola Alapizco <paolac.alapizco@gmail.com>
2019-04-16 13:47:11 -0600

    Clase de calbacks y promesas node

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call')
+
+console.log(call.sync('Paho'))
+
+call.withCallback('Paho Alapizco', call.sync)
+
+call.withPromise('Pahola C. Alapizco')
+  .then( nombre => console.log(nombre) )
--- src/call.js
+++ src/call.js
@@ -1,17 +1,17 @@
 module.exports = {
     sync: function(nombre) {
-        return "Hola ${nombre}! Bienvenido a Node.js";
+        return `Hola ${nombre}! Bienvenido a Node.js`;
     },
     withCallback: function(nombre, cb) {
         setTimeout(()=>{
-            cb(nombre)
-        }, 5000);
+            console.log(cb(nombre))
+        }, 2000);
     },
     withPromise: function(nombre) {
         return new Promise((resolve, reject)=>{
             setTimeout(()=>{
                 resolve(nombre);
-            }, 5000); 
+            }, 1500); 
         })
     }
 }
