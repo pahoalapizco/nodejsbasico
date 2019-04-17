bb4dff78631eee3e16e60907df71e41d69969f22
Paola Alapizco <paolac.alapizco@gmail.com>
2019-04-17 10:49:27 -0600

    Ejemplo para leer datos en consola

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const q = [
+  '¿Cual es tu nombre? ',
+  '¿Cual es tu primer apellido? ',
+  '¿Que edad tienes? '
+]
+
+const askQuestion = (rl, question) => {
+  return new Promise((res, rej) => {
+    rl.question(question, answer => {
+      res(answer)
+    })
+  })
+}
+
+const ask = questions => {
+  return new Promise(async resolve => {
+    let results = []
+    for(let i=0; i < questions.length; i++){
+      const result = await askQuestion(rl, questions[i])
+      results.push(result)
+    }
+    rl.close()
+    resolve(results)
+  })
+}
+
+ask(q)
+  .then( q => {
+    console.log(`Hola ${q[0]} ${q[1]}, tu edad es de ${q[2]} años`)
+  })
