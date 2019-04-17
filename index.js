57601403719c0e96b1e3832b605d09390c649d83
Paola Alapizco <paolac.alapizco@gmail.com>
2019-04-16 18:01:18 -0600

    Primer ejemplo de lectura y escritura de archivos

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require('fs')
+const ops = require('./src/fileops')
+let incrementsValues
+
+fs.readFile('./resources/number.txt',
+  'utf-8', (err, text) => {
+    if(err) throw err
+    const numbers = text.split('\n').map(n=>Number(n))
+    incrementsValues = ops.incrementValues(numbers)
+    fs.writeFile('./resources/numberNvo.txt',
+      incrementsValues.join('\n'), 
+      (err, result)=>{
+        if(err) throw err
+      })
+  })
