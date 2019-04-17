7478008002ad30c8149c9b4fa74b2510028b23db
Paola Alapizco <paolac.alapizco@gmail.com>
2019-04-17 11:10:31 -0600

    Ejemplo: Trabajando con el child process de node

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process')
+
+const execCommand = command => {
+  cp.exec(command, (err, stdout, stderr) => {
+    if(err){
+      console.log(`Error: ${err}`)
+      return
+    }
+
+    if(stdout) console.log(`Standar out: ${stdout}`)
+    if(stderr) console.log(`Standar err: ${stderr}`)
+  })
+}
+
+execCommand('ls')
